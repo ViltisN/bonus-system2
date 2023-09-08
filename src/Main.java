fbf7316c9c72fe6cf80a5925e471906428c9f078
Netology <viltis.gfx@gmail.com>
2023-09-08 21:30:03 +0300

    code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double currentBalance = 100;
+        double depositAmount = 1100;
+        double minDepositAmount = 1000;
+        int bonusAmount = 0;
+        boolean meetsMinimumDeposit = depositAmount >= minDepositAmount;
+        if (meetsMinimumDeposit) {
+            bonusAmount = (int) (depositAmount / minDepositAmount);
+            boolean hasFractionalPart = depositAmount % minDepositAmount != 0;
+            if (hasFractionalPart) {
+                bonusAmount++;
+            }
+        }
+        double finalBalance = currentBalance + depositAmount + bonusAmount;
+        System.out.println("Итоговый счёт: " + finalBalance + " рублей");
+        System.out.println("Количество бонусных рублей: " + bonusAmount);
     }
 }
