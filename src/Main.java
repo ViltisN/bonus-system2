361c9e8dea0b69b25862275793dd7750d097c3ad
Netology <viltis.gfx@gmail.com>
2023-09-13 14:41:04 +0300

    refactor3

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,18 @@ public class Main {
         double currentBalance = 100;
         double depositAmount = 1100;
         double minDepositAmount = 100;
-        int bonusAmount = 0;
-        boolean meetsMinimumDeposit = depositAmount >= minDepositAmount;
-        if (meetsMinimumDeposit) {
-            bonusAmount = (int) (depositAmount / minDepositAmount);
+
+        if (depositAmount >= minDepositAmount) {
+            int bonusAmount = (int) (depositAmount / minDepositAmount);
+            double finalBalance = currentBalance + depositAmount + bonusAmount;
+
+            System.out.println("Итоговый счёт: " + finalBalance + " рублей");
+            System.out.println("Количество бонусных рублей: " + bonusAmount);
+        } else {
+            double finalBalance = currentBalance + depositAmount;
+
+            System.out.println("Итоговый счёт: " + finalBalance + " рублей");
+            System.out.println("Количество бонусных рублей: 0");
         }
-        double finalBalance = currentBalance + depositAmount + bonusAmount;
-        System.out.println("Итоговый счёт: " + finalBalance + " рублей");
-        System.out.println("Количество бонусных рублей: " + bonusAmount);
     }
 }
